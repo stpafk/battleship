6e6851490403f13eeb65583b586df0418530034c
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-06-12 12:55:44 +0000

    ship length

--- src/gameboard.js
+++ src/gameboard.js
@@ -16,26 +16,53 @@ function gameBoard() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
     
+    function placeShip(ship, direction, row, column) {
 
-    function placeShip(shipLength, direction, row, column) {
+        let shipLength = 0;
+
+        switch(ship) {
+            case "Destroyer": 
+                shipLength = 2
+                break;
+            case "Submarine":
+                shipLength = 3;
+                break;
+            case "Cruiser": 
+                shipLength = 3;
+                break;
+            case "Battleship":
+                shipLength = 4;
+                break;
+            case "Carrier":
+                shipLength = 5;
+                break;
+        }
 
         if (direction === "vertical") {
 
             for (let i = 0; i < shipLength; i++) {
-                grid[row + i][column] = "~";
+                grid[row + i][column] = ship;
             }
         } else {
             
             for (let i = 0; i < shipLength; i++)
-            grid[row][column + i] = "~"; 
+                grid[row][column + i] = ship; 
             
         }
+    }
 
+    function receiveAttack(x, y) {
 
+        if (grid[x][y] === "Attacked") {
+            return;
+        } else {
+            grid[x][y] = "Attacked";
+        }
     }
 
     return {
-        placeShip
+        placeShip,
+        receiveAttack
     }
 
 
